be352ee8728f5555c4609e00c2a28df314bf1e04
collinewait <collinewait17@gmail.com>
2019-06-11 08:17:32 +0300

    add styling to the header component

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Stylesheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../sections/Header';
 
 export class Home extends React.Component {
     render(){
         return(
             <View>
+                <Header message='Press to login' />
                 <Text>This is the Home Page</Text>
                 <Text>Just some other lines</Text>
                 <Text>To display some text</Text>
-                <Header message='Press to login' />
             </View>
         );
     }
