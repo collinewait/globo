34d877d2626ac598094cace200f6fb79d72180d8
collinewait <collinewait17@gmail.com>
2019-06-11 07:55:30 +0300

    use props and state

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Stylesheet, Text, View } from 'react-native';
+import { Header } from '../sections/Header';
 
 export class Home extends React.Component {
     render(){
@@ -8,6 +9,7 @@ export class Home extends React.Component {
                 <Text>This is the Home Page</Text>
                 <Text>Just some other lines</Text>
                 <Text>To display some text</Text>
+                <Header message='Press to login' />
             </View>
         );
     }
