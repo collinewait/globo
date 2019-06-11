b578beadb3f69f0a56cc8522610910fe6bcbba49
collinewait <collinewait17@gmail.com>
2019-06-11 07:33:34 +0300

    use the view component

---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Stylesheet, Text, View } from 'react-native';
+
+export class Home extends React.Component {
+    render(){
+        return(
+            <View>
+                <Text>This is the Home Page</Text>
+                <Text>Just some other lines</Text>
+                <Text>To display some text</Text>
+            </View>
+        );
+    }
+}
